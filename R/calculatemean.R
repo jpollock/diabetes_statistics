153a005178fec959ce4163f4f4ae8ef3dca8bdcf
Jeremy Pollock <jpollock911@gmail.com>
2018-05-08 13:29:54 -0700

    adding more

--- R/calculatemean.R
+++ R/calculatemean.R
@@ -6,7 +6,15 @@
 #' @param values your name. Required.
 calculatemean <- function(values){
     Mean = mean(values, na.rm = TRUE)
+    SD = sd(values, na.rm = TRUE)
+    Median = median(values, na.rm = TRUE)
+    CV = 100 * (sd(df$Glucose, na.rm = TRUE)/mean(df$Glucose, na.rm = TRUE))
+    M100 = mean(1000*abs(log(df$Glucose/100, 10)))
+    J = 0.001*(Mean + SD)*(Mean + SD)
+
     list(
-        message = paste("hello", Mean, "! This is", R.Version()$version.string)
+        mean = paste(Mean),
+        sd = paste(SD),
+
     )
 }

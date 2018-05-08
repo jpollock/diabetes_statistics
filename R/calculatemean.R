6215b36b8af49454bdf8abeb22fc4c0551a3d972
Jeremy Pollock <jpollock911@gmail.com>
2018-05-08 13:04:27 -0700

    adding first stats function

---
+++ R/calculatemean.R
@@ -0,0 +1,12 @@
+#' Calculate Mean
+#' 
+#' Basic hello world function to be called from the demo app
+#' 
+#' @export
+#' @param values your name. Required.
+calculateMean <- function(values){
+    Mean = mean(values, na.rm = TRUE)
+    list(
+        message = paste("hello", Mean, "! This is", R.Version()$version.string)
+    )
+}

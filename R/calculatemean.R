cb1264774aaf3fa4cfa3a30df9bc5e19f00c43c1
Jeremy Pollock <jpollock911@gmail.com>
2018-05-08 13:34:37 -0700

    cleanup

--- R/calculatemean.R
+++ R/calculatemean.R
@@ -8,9 +8,9 @@ calculatemean <- function(values){
     Mean = mean(values, na.rm = TRUE)
     SD = sd(values, na.rm = TRUE)
     Median = median(values, na.rm = TRUE)
-    CV = 100 * ($SD/$Mean)
+    CV = 100 * (SD/Mean)
     M100 = mean(1000*abs(log(values/100, 10)))
-    J = 0.001*($Mean + $SD)*($Mean + $SD)
+    J = 0.001*(Mean + SD)*(Mean + SD)
 
     list(
         mean = paste(Mean),

8f0d966e7260d2d29848c675c587e95b172ea327
Jeremy Pollock <jpollock911@gmail.com>
2018-05-08 13:06:22 -0700

    adding first stats function

--- R/calculatemean.R
+++ R/calculatemean.R
@@ -4,7 +4,7 @@
 #' 
 #' @export
 #' @param values your name. Required.
-calculateMean <- function(values){
+calculatemean <- function(values){
     Mean = mean(values, na.rm = TRUE)
     list(
         message = paste("hello", Mean, "! This is", R.Version()$version.string)
